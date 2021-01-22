a9b02e7919f739251fad80b909a7834e55098f58
abhi213 <akaletlasub@gmail.com>
2021-01-22 17:51:57 +1300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -10,14 +10,24 @@ var x2;
 var scrollSpeed = 0;
 var foodImage;
 var eagleImage;
-var life = 5;
+var life = 3;
+var birdLife1Img,birdLife2Img, birdLife3Img, bird1, bird2, bird3;
+var birds = [];
+var index = 3;
+var gameOverSound, checkPointSound, eatingSound;
 
 function preload(){
   birdImage = loadImage("bird.png");
-  bgImage = loadImage("background.jpg");
+  bgImage = loadImage("bg.jpg");
   pillarImage = loadImage("pillar.png"); 
   foodImage = loadImage("food.png");
   eagleImage = loadImage("eagle.png"); 
+  birdLife1Img = loadImage("bird.png");
+  birdLife2Img = loadImage("bird.png");
+  birdLife3Img = loadImage("bird.png");
+  gameOverSound = loadSound("game over.wav");
+  checkPointSound = loadSound("checkpoint.wav");
+  eatingSound = loadSound("eating.wav");
 }
 
 function setup(){
@@ -32,6 +42,23 @@ function setup(){
   bird.addImage(birdImage);
   bird.scale = 0.2;
   bird.setCollider("circle", 0, 0, 150);
+
+  bird1 = createSprite(500, 100);
+  bird1.addImage(birdImage);
+  bird1.scale = 0.08;
+
+  bird2 = createSprite(550, 100);
+  bird2.addImage(birdImage);
+  bird2.scale = 0.08;
+
+  bird3 = createSprite(600, 100);
+  bird3.addImage(birdImage);
+  bird3.scale = 0.08;
+
+  birds.push(bird3);
+  birds.push(bird2);
+  birds.push(bird1);
+  
   obstacleGroup = new Group();
   foodGroup = new Group();
   eagleGroup = new Group();  
@@ -39,8 +66,11 @@ function setup(){
 
 function draw(){
   background(0);
-  image(bgImage, x1, 0, width, height);
-  image(bgImage, x2, 0, width, height);
+  if(gameState === "START"){
+    scrollSpeed = 0;
+  }
+  image(bgImage, x1, 0, width + 10, height);
+  image(bgImage, x2, 0, width + 10, height);
    x1 -= scrollSpeed;
    x2 -= scrollSpeed;
   if (x1 < -width){
@@ -52,18 +82,21 @@ function draw(){
   drawSprites();
   if(keyDown("space") && gameState === "START"){
     gameState = "PLAY"
-    scrollSpeed = 2;
+    foodGroup.destroyEach();
+    obstacleGroup.destroyEach();
+    eagleGroup.destroyEach();
+        
   }
   textSize(20);
   fill("black");
   text("Survival Time : " + survivaldisplay, 800, 100);
   text("Score : " + score, 100, 100);
-  text("life : " + life, 500, 100);
   if( gameState === "PLAY"){
+    scrollSpeed = 2 + survivaldisplay/10;
     survivalTime = survivalTime + 0.1;
     survivaldisplay = Math.round(survivalTime);
     if(survivaldisplay % 100 === 0 && survivaldisplay > 0){
-      scrollSpeed = scrollSpeed + 1;
+      checkPointSound.play();
     }
     if(keyDown("UP_ARROW")){
       bird.velocityY = -5;
@@ -73,28 +106,36 @@ function draw(){
 //      bg.x = 3*bg.width/2;
 //  }
 
-   if(bird.isTouching(foodGroup)){
-     foodGroup.destroyEach();
-     score = score + 1;
-   }
-  if(score > 2){
-    spawnEagle();
-      if(bird.isTouching(eagleGroup)){
-        life = life - 1;
-        gameState = "START";
-        bird.velocityY = 0;
-      }
+  if(bird.isTouching(foodGroup)){
+    foodGroup.destroyEach();
+    score = score + 1;
+    eatingSound.play();
   }
 
-  if(bird.isTouching(obstacleGroup)|| bird.y > 700){
+  if(score > 2){
+   spawnEagle();
+  }
+
+  if(bird.isTouching(obstacleGroup) || bird.y > 700 || bird.isTouching(eagleGroup)){
     life = life - 1;
+    index = index - 1;
+    birds[index].destroy();
     gameState = "START";
     bird.velocityY = 0;
+    obstacleGroup.setVelocityXEach(0);
+    eagleGroup.setVelocityXEach(0);
+    foodGroup.setVelocityXEach(0);
+    obstacleGroup.setLifetimeEach(-1);
+    eagleGroup.setLifetimeEach(-1);
+    foodGroup.setLifetimeEach(-1);
   }
+
   spawnObstacles();
   spawnFood();
+
   if (life <= 0 ){
     gameState = "END";
+    gameOverSound.play();
   }
   }else if(gameState === "END"){
     bird.velocityY = 0;
@@ -114,17 +155,18 @@ function spawnObstacles(){
    if(frameCount % 200 === 0){
     var obstacle = createSprite(1000, 100, 10, 10);
     obstacle.addImage(pillarImage);
-    obstacle.velocityX = -5;
+    obstacle.velocityX = -5 - survivaldisplay / 10;
+    console.log(obstacle.velocityX);
     obstacle.scale = 0.5;
     obstacle.setCollider("rectangle", 0, 0, obstacle.width - 300, obstacle.height);
     var obstacle2 = createSprite(1000, 100, 10, 10);
     obstacle2.addImage(pillarImage);
-    obstacle2.velocityX = -5;
+    obstacle2.velocityX = -5 - survivaldisplay / 10;
     obstacle2.scale = 0.5;
     obstacle.debug = true;
     obstacle2.debug = true;
     obstacle2.setCollider("rectangle", 0, 0, obstacle2.width - 300, obstacle2.height);
-  var position = Math.round(random(1, 2));
+/*  var position = Math.round(random(1, 2));
       switch(position){
         case 1: obstacle.y = 100;
         obstacle2.y = 500;
@@ -133,23 +175,33 @@ function spawnObstacles(){
         obstacle2.y = 100
         break;
         default: break;
-      }
+      }*/
       var size = Math.round(random(1, 5));
       switch(size){
         case 1: obstacle.scale = 0.15;
         obstacle2.scale = 0.15;
+        obstacle.y = 30;
+        obstacle2.y = 570;
         break;
         case 2: obstacle.scale = 0.25;
         obstacle2.scale = 0.25;
+        obstacle.y = 50;
+        obstacle2.y = 550;
         break;
         case 3: obstacle.scale = 0.35;
         obstacle2.scale = 0.35;
+        obstacle.y = 70;
+        obstacle2.y = 530;
         break;
         case 4: obstacle.scale = 0.45;
         obstacle2.scale = 0.45;
+        obstacle.y = 90;
+        obstacle2.y = 510;
         break;
         case 5: obstacle.scale = 0.55;
         obstacle2.scale = 0.55;
+        obstacle.y = 110;
+        obstacle2.y = 490;
         break;
         default: break;
       }
@@ -166,7 +218,7 @@ function spawnFood(){
     var food = createSprite(1000, 50);
     food.addImage(foodImage);
     food.y = Math.round(random(100, 500));
-    food.velocityX = -5;
+    food.velocityX = -5 - survivaldisplay / 10;
     food.scale = 0.1;
     foodGroup.add(food);
     food.lifetime = 250;
@@ -178,9 +230,10 @@ function spawnEagle(){
     var eagle = createSprite(1000, 50);
     eagle.addImage(eagleImage);
     eagle.y = Math.round(random(100, 500));
-    eagle.velocityX = -9;
+    eagle.velocityX = -9 - survivaldisplay / 10;
     eagle.scale = 1;
     eagleGroup.add(eagle);
     eagle.lifetime = 250;
+    eagle.setCollider("circle", 0, -20, 20);
   }   
 }
